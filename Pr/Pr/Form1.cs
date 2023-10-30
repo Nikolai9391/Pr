a44315d0b91dcd41b8a7ada224f6142c9797a6c8
Your Name <you@example.com>
2023-10-30 14:43:19 +0300

    first commit

---
+++ Pr/Pr/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Pr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x, y, z;
+            x = Convert.ToDouble(textBox1.Text);
+            y = Convert.ToDouble(textBox2.Text);
+            z = Convert.ToDouble(textBox3.Text);
+            double a = Math.Log10(y ^ (-(Math.Sqrt(x)))*(x - y / 2) + (Math.Pow(Math.Sin(Math.Cos(z)/Math.Sin(z)),2);
+
+
+        }
+    }
+}
